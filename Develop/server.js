c3ba887123840e3c560a9edf753d1eba99c5ee73
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-08-01 21:43:34 -0400

    initial commit. also added a server.js file

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const notes_db = require('./db/db');
+
+const PORT = 3002;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+  });
+
+app.get('/api/db', (req, res) => res.json(notes_db));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
